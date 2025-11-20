e44b445315cf29fa16b03dde21f10b0ef4885515
txrunishere <tsoni9286@gmail.com>
2025-11-21 01:22:22 +0530

    remove session context

--- src/context/sessionContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const TokenContext = createContext({ token: "", tokenLoading: false });
-
-export const TokenContextProvider = TokenContext.Provider;
-
-export function useToken() {
-  return useContext(TokenContext);
-}
