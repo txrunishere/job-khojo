fb54388b52783a33c06749b8d5b3c884c3189a0f
txrunishere <tsoni9286@gmail.com>
2025-11-22 21:55:27 +0530

    improve heading component

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,6 +1,13 @@
-export const Heading = ({ children }) => {
+import { cn } from "@/lib/utils";
+
+export const Heading = ({ children, className = "" }) => {
   return (
-    <h1 className="mx-auto max-w-md bg-linear-to-r from-neutral-400 to-neutral-300 bg-clip-text text-center text-4xl font-bold text-transparent sm:max-w-xl sm:text-6xl">
+    <h1
+      className={cn(
+        "mx-auto max-w-md bg-linear-to-r from-neutral-400 to-neutral-300 bg-clip-text text-center text-4xl font-bold text-transparent sm:max-w-xl sm:text-6xl",
+        className,
+      )}
+    >
       {children}
     </h1>
   );
